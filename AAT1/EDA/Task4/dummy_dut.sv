83bfbd9467d9045d93b1c3649a175d8ca8c52986
Pai-Pavan <pavanpaiblr@gmail.com>
2026-02-13 23:54:56 +0530

    Create dummy_dut.sv

---
+++ AAT1/EDA/Task4/dummy_dut.sv
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------
+//File       : dummy_dut.sv
+//Author     : Pavan Pai/1BM23EC179
+//Created    : 2026-02-10
+//Module     : dummy_dut
+//Project    : SystemVerilog and Verification (23EC6PE2SV)
+//Faculty    : Prof. Ajaykumar Devarapalli
+//Description: A placeholder dummy DUT for the class-based Packet verification lab.
+//------------------------------------------------------------------------------
+
+module dummy_dut;
+endmodule
